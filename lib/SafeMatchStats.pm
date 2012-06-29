3a6a10f336c661fb5cf01753f07debd746f30498
David Oswald <daoswald@gmail.com>
2012-06-28 19:34:40 -0700

    Working.

--- lib/SafeMatchStats.pm
+++ lib/SafeMatchStats.pm
@@ -5,145 +5,105 @@ use Sub::Quote;
 
 use v5.14;
 use utf8;
+use feature qw( unicode_strings );
 
 use Try::Tiny;
+use Capture::Tiny qw( capture_merged );
 use Safe;
 use Carp;
 
-use YAPE::Regex::Explain;
-
 our $VERSION = 0.01;
 
-has regexp_obj => (
-    is => 'ro',
-);
-
-has explanation => (
-    is  => 'ro',
-);
-
-
-has prematch    => (
-    is => 'rw',
-);
-
-has match       => (
-    is => 'rw',
-);
-
-has postmatch   => (
-    is  => 'rw',
-);
-
-has carat_n     => (
-    is  => 'rw',
-);
-
-has digits      => (
-    is  => 'rw',
-
-has array_minus => (
-    is  => 'rw',
-);
-
-has array_plus  => (
-    is  => 'rw',
-)
-
-has hash_minus  => (
-    is  => 'rw',
-);
-
+has regexp_obj  => ( is => 'ro' );
+has prematch    => ( is => 'rw' );
+has match       => ( is => 'rw' );
+has matched     => ( is => 'rw' );
+has postmatch   => ( is => 'rw' );
+has carat_n     => ( is => 'rw' );
+has digits      => ( is => 'rw' );
+has array_minus => ( is => 'rw' );
+has array_plus  => ( is => 'rw' );
+has hash_minus  => ( is => 'rw' );
+has hash_plus   => ( is => 'rw' );
+has target      => ( is => 'rw' );
 
 sub BUILDARGS {
-    my( $class, @args ) = @_;
+    my ( $class, @args ) = @_;
     my $args_hash = $args[0];
     croak "Must pass a hashref to $class\->new()."
-        if ref $args_hash ne 'HASH';
-    croak "Usage: $class\->new( { regexp_str => 'PATTERN' " .
-          "[, regexp_mods => '[msixadlu^-]+' } )"
-        if ! exists $args_hash->{regexp_str};
-    my $regexp_obj
-        = $class->_safe_qr( $args_hash->{regexp_str}, $args_hash->{regexp_mods} );
+      if ref $args_hash ne 'HASH';
+    croak "Usage: $class\->new( { regexp_str => 'PATTERN' "
+      . "[, regexp_mods => '[msixadlu^-]+' } )"
+      if !exists $args_hash->{regexp_str};
+    my $regexp_obj =
+      $class->_safe_qr( $args_hash->{regexp_str}, $args_hash->{regexp_mods} );
     croak "Couldn't generate a valid regexp object."
-        if ! ref $regexp_obj eq 'Regexp';
-    my $explanation;
-    try {
-        $explanation = YAPE::Regex::Explain->new($regexp_obj)->explain;
-#        $explanation =~ s/^The regular expression:\s+matches as follows:\s+//;
-    } catch {
-        $explanation = q{};
-    };
+      if !ref $regexp_obj eq 'Regexp';
     return {
-        regexp_obj  => $regexp_obj,
-        explanation => $explanation,
+        regexp_obj => $regexp_obj,
+        matched    => 0,
     };
 }
 
-sub match {
-    my( $self, $target ) = @_;
-    my $re_obj = $self->regexp_obj;
-    return scalar $self->_safe_match_gather( $target, $re_obj );
+sub do_match {
+    my ( $self, $target ) = @_;
+    return scalar $self->_safe_match_gather( $target, $self->regexp_obj );
 }
-
 
 sub _safe_qr {
-    my( $self, $regexp, $modifiers ) = @_;
-    $regexp    = $self->_sanitize_re_string( $regexp );
-    $modifiers = $self->_sanitize_re_modifiers( $modifiers );
+    my ( $self, $regexp, $modifiers ) = @_;
     my $compartment = Safe->new;
-    ${ $compartment->varglob('regexp') } = $regexp;
-    my $re_obj = $compartment->reval( 'my $safe_reg = qr/$regexp/;' );
-    return if $@;   # Return undef if 'reval' caught an exception.
-    return $re_obj; # Otherwise return a regexp object.
+    ${ $compartment->varglob('regexp') } = $self->_sanitize_re_string($regexp);
+    ${ $compartment->varglob('modifiers') } =
+      $self->_sanitize_re_modifiers($modifiers);
+    my $re_obj =
+      $compartment->reval('my $safe_reg = qr/(?$modifiers:$regexp)/;');
+    return if $@;    # Return "undef" if 'reval' caught an exception.
+    return $re_obj;  # Otherwise return a regexp object.
 }
 
 sub _sanitize_re_modifiers {
-    my( $self, $modifiers ) = @_;
-    return '' if ! defined $modifiers;
+    my ( $self, $modifiers ) = @_;
+    return '' if !defined $modifiers;
     $modifiers =~ tr/msixadlu^-//cd;
     my @modifiers = split //, $modifiers;
     my %seen;
-    return join '', grep { ! $seen{$_}++ } @modifiers;
+    return join '', grep { !$seen{$_}++ } @modifiers;
 }
+
 sub _sanitize_re_string {
     my ( $self, $re_string ) = @_;
     no warnings 'qw';
-    my @possible_varnames = qw%
-        \$\^\w          \@ENV              \$ENV
-        \$[0()<>#!+-]   \$\{[\w()<>+-]}    \@\{\w+}
-    %;
-    foreach my $bad_pattern ( @possible_varnames  ) {
-        $re_string =~ s/(?<!\\)($bad_pattern)/\\$1/g;
-    }
+    my @bad_varnames = qw%       \$\^\w          \@ENV              \$ENV
+      \$[0()<>#!+-]   \$\{[\w()<>+-]}    \@\{\w+}      %;
+    $re_string =~ s/(?<!\\)($_)/\\$1/g foreach @bad_varnames;
     return $re_string;
 }
 
 sub _safe_match_gather {
     my ( $self, $target ) = @_;
+    $self->target($target);
     my $re_obj = $self->regexp_obj;
     $self->matched(0);
-    try {
-        $self->matched( scalar $target =~ m/$re_obj/ );
-    } catch {
-        $self->matched( undef );
+    try { $self->matched( scalar $target =~ m/$re_obj/ ); }
+    catch {
+        $self->matched(undef);
         warn "Problem in matching: $_";
     };
-    if( $target =~ m/$re_obj/ ) {
-            $self->matched( 1 );
-            $self->digits(
-                [ map { substr $target, $-[$_], $+[$_] - $-[$_] } 0 .. $#- ]
-            );
-            $self->hash_plus(   { %+ }        );
-            $self->hash_minus(  { %- }        );
-            $self->prematch(    ${^PREMATCH}  );
-            $self->match(       ${^MATCH}     );
-            $self->postmatch(   ${^POSTMATCH} );
-            $self->carat_n(     $^N           );
-            $self->array_minus( [ @- ]        );
-            $self->array_plus(  [ @+ ]        );
-    }
-    return $self->matched
+    my $matched;
+    $matched = $self->matched(1) if $target =~ m/$re_obj/;
+    $self->digits(
+        [ map { substr $target, $-[$_], $+[$_] - $-[$_] } 0 .. $#- ] );
+    $self->hash_plus( $matched   ? {%+}          : undef );
+    $self->hash_minus( $matched  ? {%-}          : undef );
+    $self->prematch( $matched    ? ${^PREMATCH}  : undef );
+    $self->match( $matched       ? ${^MATCH}     : undef );
+    $self->postmatch( $matched   ? ${^POSTMATCH} : undef );
+    $self->carat_n( $matched     ? $^N           : undef );
+    $self->array_minus( $matched ? [@-]          : undef );
+    $self->array_plus( $matched  ? [@+]          : undef );
+
+    return $self->matched;
 }
 
 1;
--- myapp.pl
+++ myapp.pl
@@ -5,27 +5,51 @@ use lib 'lib';
 
 use SafeMatchStats;
 
+app->secret('Some Session Secret');
+
+app->defaults(
+    {
+        map { $_, undef }
+          qw( match_success   prematch    match
+              postmatch       carat_n     digits
+              array_minus     array_plus  hash_minus
+              hash_plus                               )
+    }
+);
+
+plugin 'PoweredBy';
+plugin 'PODRenderer';
+
 any '/' => sub {
     my $self = shift;
-    $self->stash(
-        match_success => undef,     captures => undef,
-        explanation   => undef,     graphviz => undef
-    );
-    if( $self->param('regexp') ) {
-        my $re = SafeMatchStats->new( {
-            regexp_str => $self->param('regexp'),
-            regexp_mods => $self->param('modifiers')
-        } );
-        my $success = $re->match( $self->param('target') );
-        $self->stash( match_success => $success         );
-        $self->stash( captures      => $re->captures    );
-        $self->stash( explanation   => $re->explanation );
+    if ( $self->param('regexp') ) {
+        my $re = SafeMatchStats->new(
+            {
+                regexp_str  => $self->param('regexp')    || '',
+                regexp_mods => $self->param('modifiers') || ''
+            }
+        );
+        $self->stash(
+            match_success => $re->do_match( $self->param('target') ) );
+        $self->stash(
+            {
+                prematch    => $re->prematch,
+                match       => $re->match,
+                postmatch   => $re->postmatch,
+                carat_n     => $re->carat_n,
+                digits      => $re->digits,
+                array_minus => $re->array_minus,
+                array_plus  => $re->array_plus,
+                hash_minus  => $re->hash_minus,
+                hash_plus   => $re->hash_plus,
+            }
+        );
     }
     $self->render('index');
 };
 
-
 app->start;
+
 __DATA__
 
 @@ index.html.ep
@@ -39,82 +63,101 @@ __DATA__
     The regexp engine used is compatible with Perl <%= " $^V" =%>.
 </p>
 %= form_for '/' => ( method => 'post' ) => begin
-    <p>
-        Target string:
-        <br />
-        <%=
-            text_area 'target', cols => 80, placeholder => 'Target string',
-            maxlength => 2048
-        =%>
-    </p>
-    <p>
-        Regular Expression:
-        <br />
-        <%=
-            text_area 'regexp', cols => 80, placeholder => 'Regular Expression',
-            maxlength => 2048
-        =%>
-        <br />
-        <small>Example: To test "<code>m/pattern/</code>", enter
-        "<code>pattern</code>" (without quotes).</small>
-    </p>
-    <p>
-        Modifiers:
-        <br />
-        <%= text_field 'flags', cols        => '30', 
-            placeholder => 'Valid Modifiers: [msixadlu]',
-            maxlength   => '8'
-        %>
-        %= submit_button
-    </p>
+  <p>
+    Target string:
+    <br />
+    <%= text_area 'target', cols        => 80,
+                            placeholder => 'Target string',
+                            maxlength   => 2048
+    =%>
+  </p>
+  <p>
+    Regular Expression:
+    <br />
+    <%=
+      text_area 'regexp', cols        => 80,
+                          placeholder => 'Regular Expression',
+                          maxlength   => 2048
+    =%>
+    <br />
+      <small>
+        Example: To test "<code>m/pattern/</code>", enter
+        "<code>pattern</code>" (without quotes).
+      </small>
+  </p>
+  <p>
+    Modifiers:
+    <br />
+    <%= text_field 'modifiers', cols        => '30', 
+                                placeholder => 'Valid Modifiers: [msixadlu]',
+                                maxlength   => '10'
+    %>
+    %= submit_button
+  </p>
 %end
+
 <article>
 %== '<h2>Match!</h2>' if $match_success
-%== '<h2>No match!</h2>' if ! $match_success && defined $match_success
+%== '<h2>No match!</h2>' if defined $match_success && ! $match_success
 %== '<h2>Invalid regular expression!</h2>' if param('regexp') && ! defined $match_success
 </article>
-<% if( ref( $captures ) eq 'HASH' ) {
+<% if( $match_success ) {
     local $" = ',';
 =%>
-    <article>
-        <h3>Capture variables:</h3>
-        <pre>
-            <%= "\${^PREMATCH}   => $captures->{'${^PREMATCH}'}\n"  =%>
-            <%= "\${^MATCH}      => $captures->{'${^MATCH}'}\n"     =%>
-            <%= "\${^POSTMATCH}  => $captures->{'${^POSTMATCH}'}\n" =%>
-            <%= "\$^N            => $captures->{'$^N'}\n"           =%>
-            <% foreach my $digit ( 1 .. $#{$captures->{'$<digits>'}} ) { =%>
-                <%= "\$$digit: $captures->{'$<digits>'}[$digit]\n"  =%>
-            <% } =%>
-            <% foreach my $name ( keys %{$captures->{'$+{name}'}} ) { =%>
-                <%= "\$+{$name} => $captures->{'$+{name}'}{$name}\n"  =%>
-            <% } =%>
-            <% foreach my $name ( keys %{$captures->{'$-{name}'}} ) { =%>
-                <%= "\$-{$name} => (@{$captures->{'$-{name}'}{$name}})\n" =%>
-            <% } =%>
-            <%= @{$captures->{'@-'}} && "\@- => (@{$captures->{'@-'}})\n" =%>
-            <%= @{$captures->{'@+'}} && "\@+ => (@{$captures->{'@+'}})\n" =%>
-        </pre>
-    </article>
+  <article>
+    <h3>Capture variables:</h3>
+      <ul>
+        %  if( @{$digits} > 1 ) {
+          <li>Digit Captures
+            <ul>
+            %  foreach my $digit ( 1 .. $#{$digits} ) {
+              %== "<li><pre>\$$digit => $digits->[$digit]</pre></li>"
+            % }
+            </ul>
+          </li>
+        % }
+        % if( keys %{$hash_plus} ) {
+          <li>Named Captures
+            <ul>
+              % foreach my $name ( keys %{$hash_plus} ) {
+                %== "<li><pre>\$+{$name} => $hash_plus->{$name}</pre></li>"
+              % }
+            </ul>
+          </li>
+        % }
+        %== defined $prematch  && "<li><pre>\${^PREMATCH}  => $prematch</pre></li>"
+        %== defined $match     && "<li><pre>\${^MATCH}     => $match</pre></li>"
+        %== defined $postmatch && "<li><pre>\${^POSTMATCH} => $postmatch</pre></li>"
+        %== defined $carat_n   && "<li><pre>\$^N           => $carat_n</pre></li>"
+        % foreach my $name ( keys %{$hash_minus} ) {
+          %== "<li><pre>\$-{$name} => (@{$hash_minus->{$name}})</pre></li>"
+        % }
+        %== @{$array_minus} && "<li><pre>\@- => (@{$array_minus})</pre></li>"
+        %== @{$array_plus}  && "<li><pre>\@+ => (@{$array_plus})</pre></li>"
+    </ul>
+  </article>
 % }
-% if( param('regexp') ) {
-    <article>
-        <p>
-            <h3>Regex Explanation:</h3>
-            <p>
-                %== length $explanation &&  "<pre>$explanation</pre>"
-            </p>
-            <p>
-                <small>Note: The <em>Regex Explanation</em> section doesn't
-                properly comprehend regexp constructs unique to Perl v5.10 or
-                later.</small>
-            </p>
-        </p>
-    </article>
-% }
+
+
+
+
 @@ layouts/default.html.ep
 <!DOCTYPE html>
 <html>
-    <head><title><%= title %></title></head>
-    <body><%= content %></body>
+  <head><title><%= title %></title></head>
+  <body>
+    <article>
+      <header>
+        [
+          <%= link_to '/' => begin %>The Perl Regex Tester<% end =%> |
+          <a href='http://perldoc.perl.org/perlre.html'>perlre</a> |
+          <a href='http://perldoc.perl.org/perlrequick.html'>perlrequick</a> |
+          <a href='http://perldoc.perl.org/perlretut.html'>perlretut</a> |
+          <a href='http://perldoc.perl.org/perlop.html'>perlop</a> |
+          <a href='http://perldoc.perl.org/perlfaq6.html'>perlfaq6</a>
+        ]
+      </header>
+    </article>
+    <%= content %>
+  </body>
 </html>
