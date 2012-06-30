c6479a39400ccec7c97a826acb2e23512949e690
David Oswald <daoswald@gmail.com>
2012-06-30 16:14:58 -0700

    Reworked Moo code.

--- lib/SafeMatchStats.pm
+++ lib/SafeMatchStats.pm
@@ -1,7 +1,6 @@
 package SafeMatchStats;
 
 use Moo;
-use Sub::Quote;
 
 use v5.12;
 use utf8;
@@ -14,63 +13,62 @@ use Carp;
 
 our $VERSION = 0.01;
 
-has regexp_obj  => ( is => 'ro' );
-has regexp_str  => ( is => 'ro' );
-has prematch    => ( is => 'rw' );
-has match       => ( is => 'rw' );
-has matched     => ( is => 'rw' );
-has postmatch   => ( is => 'rw' );
-has carat_n     => ( is => 'rw' );
-has digits      => ( is => 'rw' );
-has array_minus => ( is => 'rw' );
-has array_plus  => ( is => 'rw' );
-has hash_minus  => ( is => 'rw' );
-has hash_plus   => ( is => 'rw' );
-has target      => ( is => 'rw' );
 
-sub BUILDARGS {
-    my ( $class, @args ) = @_;
-    my $args_hash = $args[0];
-    croak "Must pass a hashref to $class\->new()."
-      if ref $args_hash ne 'HASH';
-    croak "Usage: $class\->new( { regexp_str => 'PATTERN' "
-      . "[, regexp_mods => '[msixadlu^-]+' } )"
-      if !exists $args_hash->{regexp_str};
-    my $full_regex_str = $class->_gen_re_string(
-        $args_hash->{regexp_str}, $args_hash->{regexp_mods}
+# Constructor:
+has regex        => ( is => 'ro', required => 1 );  # User input (constructor).
+has modifiers    => ( is => 'ro'                );  # User input (constructor).
+
+has regexp_str   => ( is => 'rw', lazy => 1, builder => \&_gen_re_string );
+has regexp_obj   => ( is => 'rw', lazy => 1, builder => \&_safe_qr       );
+
+# Set by a call to do_match().
+has target       => ( is => 'rw', lazy => 1, default => sub { q{} } );
+has capture_dump => ( is => 'rw' );
+has matched      => ( is => 'rw', default => sub{ undef } );
+
+# Captures
+my @attribs = qw/   prematch    match       postmatch   carat_n     digits  
+                    array_minus array_plus  hash_minus  hash_plus           /;
+
+foreach my $attrib ( @attribs  ) {
+    has $attrib => ( 
+        is      => 'ro', lazy    => 1, 
+        builder => sub{ 
+            return $_[0]->matched ? $_[0]->capture_dump->{$attrib} : undef;
+        }
     );
-    my $regexp_obj =
-      $class->_safe_qr( $full_regex_str );
-    croak "Couldn't generate a valid regexp object."
-      if !ref $regexp_obj eq 'Regexp';
-    return {
-        regexp_obj => $regexp_obj,
-        regexp_str => $full_regex_str,
-        matched    => 0,
-    };
 }
 
+# Debug: The debugger dump from match.
+has debug_info  => ( is => 'rw', lazy => 1, default => \&_debug_info );
+
+around BUILDARGS => sub {
+    my ( $orig, $class, @args ) = @_;
+    return $class->$orig( @args )
+        unless @args == 1 && ! ref $args[0] eq 'HASH';
+    return $class->$orig( regex => $_[0] );
+};
 
 sub do_match {
     my ( $self, $target ) = @_;
-    return scalar $self->_safe_match_gather( $target, $self->regexp_obj );
+    $self->target( $target // $self->target );
+    return scalar $self->_safe_match_gather( $self->target, $self->regexp_obj );
 }
 
 sub _gen_re_string {
-    my( $self, $raw_re_str, $raw_mods ) = @_;
-    my $re_str = $self->_sanitize_re_string( $raw_re_str );
-    my $mod_str = $self->_sanitize_re_modifiers( $raw_mods );
+    my $self = shift;
+    my $re_str = $self->_sanitize_re_string(     $self->regex     );
+    my $mod_str = $self->_sanitize_re_modifiers( $self->modifiers );
     return "(?$mod_str:$re_str)";
 }
 
-sub _safe_qr {
-    my ( $self, $re_str ) = @_;
-    my $compartment = Safe->new;
-    ${ $compartment->varglob('regexp') } = $re_str;
-    my $re_obj =
-      $compartment->reval('my $safe_reg = qr/$regexp/;');
-    return if $@;    # Return "undef" if 'reval' caught an exception.
-    return $re_obj;  # Otherwise return a regexp object.
+sub _sanitize_re_string {
+    my ( $self, $re_string ) = @_;
+    no warnings 'qw';
+    my @bad_varnames = qw%    \$\^\w    \@ENV    \$ENV
+      \$[0()<>#!+-]    \$\{[\w()<>+^-]}    \@\{\w+}     %;
+    $re_string =~ s/(?<!\\)($_)/\\$1/g foreach @bad_varnames;
+    return $re_string;
 }
 
 sub _sanitize_re_modifiers {
@@ -82,33 +80,37 @@ sub _sanitize_re_modifiers {
     return join '', grep { !$seen{$_}++ } @modifiers;
 }
 
-sub _sanitize_re_string {
-    my ( $self, $re_string ) = @_;
-    no warnings 'qw';
-    my @bad_varnames = qw%       \$\^\w          \@ENV              \$ENV
-      \$[0()<>#!+-]   \$\{[\w()<>+-]}    \@\{\w+}      %;
-    $re_string =~ s/(?<!\\)($_)/\\$1/g foreach @bad_varnames;
-    return $re_string;
+sub _safe_qr {
+    my $self = shift;
+    my $compartment = Safe->new;
+    ${ $compartment->varglob('regexp') } = $self->regexp_str;
+    my $re_obj =
+      $compartment->reval('my $safe_reg = qr/$regexp/;');
+    return if $@;    # Return "undef" if 'reval' caught an exception.
+    return $re_obj;  # Otherwise return a regexp object.
 }
 
 sub _safe_match_gather {
-    my ( $self, $target ) = @_;
-    $self->target($target);
+    my $self = shift @_;
+    my $target = $self->target;
     my $re_obj = $self->regexp_obj;
     $self->matched(0);
     try {
         $self->matched(1) if $target =~ m/$re_obj/;
-        my $matched = $self->matched;
-        $self->digits(
-            [ map { substr $target, $-[$_], $+[$_] - $-[$_] } 0 .. $#- ] );
-        $self->hash_plus(   $matched ? {%+}          : undef );
-        $self->hash_minus(  $matched ? {%-}          : undef );
-        $self->prematch(    $matched ? ${^PREMATCH}  : undef );
-        $self->match(       $matched ? ${^MATCH}     : undef );
-        $self->postmatch(   $matched ? ${^POSTMATCH} : undef );
-        $self->carat_n(     $matched ? $^N           : undef );
-        $self->array_minus( $matched ? [@-]          : undef );
-        $self->array_plus(  $matched ? [@+]          : undef );
+        if( $self->matched ) {
+            $self->match_dump( {
+                digits    => 
+                    [ map { substr $target, $-[$_], $+[$_] - $-[$_] } 0 .. $#- ],
+                hash_plus => {%+},
+                hash_minus => {%-},
+                prematch   => ${^PREMATCH},
+                match      => ${^MATCH},
+                postmatch  => ${^POSTMATCH},
+                carat_n    => $^N,
+                array_minus => [ @- ],
+                array_plus  => [ @+ ],
+            } );
+        }
     }
     catch {
         $self->matched(undef);
@@ -118,27 +120,23 @@ sub _safe_match_gather {
     return $self->matched;
 }
 
-sub debug_info {
-    my ( $self, $target ) = @_;
-    my $re_string = $self->regexp_str;
-    my( undef, $stderr, undef )
-        = capture { $self->_do_re_debug( $target, $re_string ) };
+sub _debug_info {
+    my $self = shift;
+    my $rv;
+    my( undef, $stderr, undef ) = capture { 
+        try {
+            use re q/debug/;
+            my $regex = $self->regexp_str;
+            $rv = $self->target =~ m/$regex/;
+        }
+        catch {
+            s/\sat\s[\w.]+\sline\s\d+\.$/./;
+            print STDERR "$_\n";
+            $rv = undef;
+        };
+        $rv;
+    };
     return $stderr;
 }
-
-sub _do_re_debug {
-    my( $self, $target, $regex ) = @_;
-    my $rv;
-    try {
-        use re q/debug/;
-        $rv = $target =~ m/$regex/;
-    }
-    catch {
-        s/\sat\s[\w.]+\sline\s\d+\.$/./;
-        print STDERR "$_\n";
-    };
-    return $rv;
-}
-    
 
 1;
---
+++ t/10-safematchstats.t
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use lib 'lib';
+use Test::More;
+
+use_ok( 'SafeMatchStats' );
+
+ok(1);
+
+done_testing();
